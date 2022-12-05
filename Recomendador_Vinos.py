cd573fbf600ad19b8100d19a2c794430e73a2801
EnriqueCamposICAI <102147919+EnriqueCamposICAI@users.noreply.github.com>
2022-12-05 08:25:30 +0100

    Update Recomendador_Vinos.py

--- Recomendador_Vinos.py
+++ Recomendador_Vinos.py
@@ -1,11 +1,12 @@
-from doctest import DocFileSuite
+
 import pandas as pd
 import re
 
 
-# Implementar Regex
+
 
 def extract():
+    # Se extraen los datos del csv
     df = pd.read_csv("wines_SPA.csv", sep=",", encoding="UTF-8")
     df.drop_duplicates(inplace=True)
     print(df)
@@ -27,18 +28,31 @@ def transform(df_original, df_filtrado, vino):
     print("     7. type")
     print("     8. body (0-5)")
     print("     9. acidity (0-5)")
-
+    
+    # Se listan los filtros
+    
+    # Se pide el número de filtro al usuario
+    
     filtro_num = input("\nPor qué quiere filtrar (número)? ")
     filtro = filtros[filtro_num]
+    
     print("")
+    
     while filtro_num != "No" or df_filtrado.empty:
         valor = df_original.at[vino, filtro]
+        
+        # Se busca el valor del filtro de nuestro vino inicial
+        
+        # Si es precio, se mira un rango de 10%. El resto si es igual
         if filtro == "price":
             df_filtrado = df_filtrado[df_filtrado[filtro]
                                       <= valor*1.1]
             df_filtrado = df_filtrado[df_filtrado[filtro] >= valor*0.9]
         else:
             df_filtrado = df_filtrado[df_filtrado[filtro] == valor]
+            
+        # Si en algún momnto me paso de filtrado, hay que volver a empezar de 0
+        
         if df_filtrado.empty:
             print("Se han realizado demasiados filtros. Filtre de nuevo desde 0. ")
             df_filtrado = transform(df_original, df_filtrado_recurs, vino)
@@ -46,6 +60,7 @@ def transform(df_original, df_filtrado, vino):
         else:
             print(df_filtrado)
         filtro_num = input("Quiere añadir algún filtro más? ")
+        # Se pregunta un filtro nuevo, ssalvo que no se quiera usar más
         if filtro_num != "No":
             filtro = filtros[filtro_num]
 
@@ -65,18 +80,24 @@ if __name__ == "__main__":
     df_año = pd.DataFrame()
 
     while df_año.empty:
+        # Se pide el vino. Se puede añadir con o sin paréntesis, pero tiene que ser "," + "space"
         vino = input("\nIntroduzca vino (Bodega, Vino, Año): ")
         vino = (re.sub(r'[()]', '', vino)).split(', ')
+        
+        # Se va filtrando por bodega, vino, año
         df_bodega = df[df["winery"] == vino[0]]
         df_nombre = df_bodega[df_bodega["wine"] == vino[1]]
         df_año = df_nombre[df_nombre["year"] == vino[2]]
 
         if df_año.empty:
+            # Si no encuentra nada, hay que meterlo otra vez o no está en la base de datos
             print("\nHa habido un error. Introduzca el vino correctamente. Si es correcto, el vino no está en la base de datos")
 
     print("\nEl vino seleccionado es alguno de los siguientes:")
 
     print(df_año)
+    # Se pide qeu se seleccione el vino (suele haber filas duplicadas por vinos, pero algunas con valores Nan)
+    # Hay vinos que aparecen con valores NaN pero se ha preferido para no perder datos
     indice_v = int(input(
         "\nIntroduzca el índice de su vino (el que no contenga NaN si duplicados): "))
     indice = list(df_año.index)
